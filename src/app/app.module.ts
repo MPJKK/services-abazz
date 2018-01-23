5e6447c007dfdd536752a2131992a40615506b0c
Antti Koivumäki <anttko@ety-a2123b-31.edu.wks.metropolia.fi>
2018-01-23 10:07:04 +0200

    part a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListMediaComponent } from './list-media/list-media.component';
+import {MediaService} from './services/media.service';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListMediaComponent
+    ],
+    imports: [
+        BrowserModule, HttpClientModule
+    ],
+    providers: [MediaService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
